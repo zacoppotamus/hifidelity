1131c62b06ff9cac0a7f3272308469f7d244cb5f
Zac Ioannidis <zacoppotamus@gmail.com>
2014-04-16 03:15:09 +0300

    Debug statements

--- server.js
+++ server.js
@@ -23,3 +23,5 @@ require('./app/routes.js')(app);
 // listen (start app with node server.js) ======================================
 app.listen(port);
 console.log("App listening on port " + port);
+console.log(process.env.MONGOLAB_URI);
+console.log(process.env.MONGOHQ_URL);
