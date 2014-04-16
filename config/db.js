6bbd88d4f4ebbc45614344987d21703b15523bef
Zac Ioannidis <zacoppotamus@gmail.com>
2014-04-16 03:09:44 +0300

    Added mongo db

--- config/db.js
+++ config/db.js
@@ -1,5 +1,8 @@
 module.exports = {
 
 // the database url to connect
-    url : 'mongodb://127.0.0.1:27017/hifidelity'
+    // url : 'mongodb://127.0.0.1:27017/hifidelity'
+    url :   process.env.MONGOLAB_URI ||
+            process.env.MONGOHQ_URL ||
+            'mongodb://127.0.0.1:27017/hifidelity'
 }
